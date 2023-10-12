7b538b3db73c062c27f36526968c6c6f8ef3c930
FrancisIsaiasIrias <francis.irias@unah.hn>
2023-10-12 00:10:52 -0600

    primer carga del proyecto

---
+++ app/src/main/java/com/example/app_traex/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.app_traex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.window.SplashScreen
+import kotlin.system.measureTimeMillis
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
