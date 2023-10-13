3efc15b03444662d6e75a8a57c3286c909044b5d
Robinson <146052596+RobinsonIzcano@users.noreply.github.com>
2023-10-12 22:53:13 -0600

    diseño de login botones y cajas de txt

--- app/src/main/java/com/example/app_traex/Login.kt
+++ app/src/main/java/com/example/app_traex/Login.kt
@@ -2,12 +2,10 @@ package com.example.app_traex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.window.SplashScreen
-import kotlin.system.measureTimeMillis
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
